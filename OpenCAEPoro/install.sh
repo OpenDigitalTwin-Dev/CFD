c057ff021a56b9747877a9c0b571b4a7947dfe61
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-04-22 15:08:24 +0800

    c

--- OpenCAEPoro/install.sh
+++ OpenCAEPoro/install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+sudo apt update
 sudo apt -y install p7zip-full
 sudo apt -y install libblas-dev
 sudo apt -y install liblapack-dev
+sudo apt -y install cmake
+sudo apt -y install make
+sudo apt -y install gcc g++
 
 rm -rf examples
 7za x examples.7z -o/$PWD/examples
